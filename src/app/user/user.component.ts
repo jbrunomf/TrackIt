1beef84b9b3b36e1713f11be433cc2f94c5afac3
João Bruno <jbrunomf@outlook.com>
2025-01-01 20:11:56 -0400

    Refactor user component to use dynamic inputs/outputs.
    Replaced hardcoded user data with dynamic inputs and outputs for better reusability and maintainability. Updated the app to iterate over user data and pass appropriate properties to each user component. Emitted user selection events for more scalable event handling.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import {DUMMY_USERS} from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,13 +7,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() selected = new EventEmitter();
 
-  get imagePath(){
-    return 'assets/users/' + this.selectedUser.avatar
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser(){
-    console.log('Clicou em: ', this.selectedUser.name);
+  onSelectUser() {
+    this.selected.emit(this.id);
   }
 }
+
+
